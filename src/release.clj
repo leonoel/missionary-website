22512751d4a40d57091b0dc8fcd873b8abfea543
Léo NOEL <contact@leonoel.fr>
2023-10-23 21:25:49 +0200

    meta viewport

--- src/release.clj
+++ src/release.clj
@@ -174,6 +174,7 @@
 (defn page-head [dev?]
   [:head
    [:meta {:charset "utf-8"}]
+   [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
    [:link {:rel "preconnect" :href "https://fonts.googleapis.com"}]
    [:link {:rel "preconnect" :href "https://fonts.gstatic.com" :crossorigin "true"}]
    [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css2?family=Lexend:wght@300&family=Source+Code+Pro&display=swap"}]
